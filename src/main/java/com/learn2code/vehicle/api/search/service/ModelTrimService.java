9dc78e35beaa49c463d211dae1be830ee3d8b000
unknown <eduard.lucian.iacob@gmail.com>
2023-07-29 10:51:31 +0300

    update model and trim type

--- src/main/java/com/learn2code/vehicle/api/search/service/ModelTrimService.java
+++ src/main/java/com/learn2code/vehicle/api/search/service/ModelTrimService.java
@@ -2,6 +2,8 @@ package com.learn2code.vehicle.api.search.service;
 
 import com.learn2code.vehicle.api.search.entity.Model;
 import com.learn2code.vehicle.api.search.entity.TrimType;
+import com.learn2code.vehicle.api.search.exception.ModelNotFoundException;
+import com.learn2code.vehicle.api.search.exception.TrimTypeNotFoundException;
 
 import java.util.List;
 
@@ -10,5 +12,9 @@ public interface ModelTrimService {
     TrimType saveTrimType(TrimType trimType);
 
     List<Model> getAllModels();
+    Model modifyModel(int id, Model model) throws ModelNotFoundException;
+    TrimType modifyTrimType(int id, TrimType trimType) throws TrimTypeNotFoundException;
+    Model getModelById(int id) throws ModelNotFoundException;
+    TrimType getTrimTypeById(int id) throws TrimTypeNotFoundException;
 
 }
