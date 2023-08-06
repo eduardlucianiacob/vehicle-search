699f647be29c0c88dde099f799091079f0c1289d
unknown <eduard.lucian.iacob@gmail.com>
2023-08-06 12:13:18 +0300

    commit
    add vehicle deatails api call

---
+++ src/main/java/com/learn2code/vehicle/api/search/service/VehicleDetailService.java
@@ -0,0 +1,10 @@
+package com.learn2code.vehicle.api.search.service;
+
+import com.learn2code.vehicle.api.search.dto.VehicleDetail;
+import com.learn2code.vehicle.api.search.dto.VehicleDetailsDTO;
+
+import java.util.List;
+
+public interface VehicleDetailService {
+    VehicleDetailsDTO getAllVehicleDetails();
+}
