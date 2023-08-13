5600039bd98ae769db580a98474f2b51dfaef87e
unknown <eduard.lucian.iacob@gmail.com>
2023-08-13 11:19:45 +0300

    US16 Basic authentication

---
+++ src/main/java/com/learn2code/vehicle/api/search/entity/Account.java
@@ -0,0 +1,25 @@
+package com.learn2code.vehicle.api.search.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name="accounts")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+    private boolean enabled;
+    private String roles;
+
+}
